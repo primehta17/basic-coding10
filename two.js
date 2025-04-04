c902e01171557f497aa73fe0ddcc446b26955ab7
Priyanka Mehta <primehta17@gmail.com>
2025-04-04 11:50:00 +0530

    Variable Is undefined or null & Default Parameter

---
+++ two.js
@@ -0,0 +1,16 @@
+// JavaScript Program To Check If A Variable Is undefined or null
+// JavaScript Program to Set a Default Parameter Value For a Function
+
+
+//x=null->output:num is undefined or null
+//x->output:num is undefined or null
+//and for all datatypes :num is neither undefined or null
+function checkUndefinedornull(x){
+  if(x==null){
+    console.log("num is undefined or null");
+  }else{
+    console.log("num is neither undefined or null");
+  }
+}
+
+checkUndefinedornull();
